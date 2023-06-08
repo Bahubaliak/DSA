825403a516897a662d024a45b10de096be04aaa7
bahubaliak <bahubali@webrino.com>
2023-06-08 21:50:23 +0530

    solve array based questions

---
+++ 3.ArrayQuestions/reverseArray.js
@@ -0,0 +1,22 @@
+
+function reverseArray(n, arr) {
+  // n -> no. of elements
+  // arr -> given array
+  let start = 0
+  let end = n-1 
+  while (start < end) {
+    let temp = arr[start]
+    arr[start] = arr[end]
+    arr[end] = temp
+    
+    start++
+    end--
+  }
+  return arr
+}
+
+let answer = reverseArray(5, [50, 10, 20, 40, -30])
+
+answer.forEach( item => console.log(item + ' '))
+
+// reversed array = [-30, 40, 20, 10, 50]
