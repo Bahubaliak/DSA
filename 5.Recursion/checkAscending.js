d9ef2266ed3d2c18a0aa83cc8fb780b449bd293d
bahubaliak <bahubalikurali@gmail.com>
2023-06-20 09:53:43 +0530

    check given array is in asc or not using recursion

---
+++ 5.Recursion/checkAscending.js
@@ -0,0 +1,18 @@
+// check given array is in asc order or not using recursion
+
+var checkAscending = function (arr, index) {
+
+  if (index == arr.length-1) {
+    return true
+  }
+
+  let tempAnswer = checkAscending(arr, index+1)
+
+  let result = tempAnswer && arr[index] <= arr[index+1]
+
+  return result
+}
+
+let answer = checkAscending([1,2,3,4,5], 0)
+
+console.log(answer)
