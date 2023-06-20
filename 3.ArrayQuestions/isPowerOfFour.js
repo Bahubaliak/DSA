b369dbf550e4e8803b905b670bff54775c59681c
bahubaliak <bahubalikurali@gmail.com>
2023-06-20 21:20:20 +0530

    added is power of X algo

---
+++ 3.ArrayQuestions/isPowerOfFour.js
@@ -0,0 +1,13 @@
+// link = https://leetcode.com/problems/power-of-four/description/
+
+var isPowerOfFour = function(n) {
+  while (n > 1) {
+      n = n/4
+  }
+
+  return n === 1
+}
+
+let answer = isPowerOfFour(16)
+
+console.log(answer)
