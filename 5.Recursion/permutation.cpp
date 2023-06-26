4229635ce8e14a6f251a3e5624178e53b84b0d6e
bahubaliak <bahubalikurali@gmail.com>
2023-06-26 22:18:39 +0530

    permutatin using recursion

---
+++ 5.Recursion/permutation.cpp
@@ -0,0 +1,29 @@
+// link = https://leetcode.com/problems/permutations/
+
+class Solution {
+
+private:
+void solve(vector<int> nums, vector<vector<int>>& ans, int index) {
+    // base case
+    if(index >= nums.size()) {
+        ans.push_back(nums);
+        return;
+    }
+
+    for(int j=index; j<nums.size(); j++) {
+        //swap elements
+        swap(nums[index], nums[j]);
+        solve(nums, ans, index+1);
+        //back track
+        swap(nums[index], nums[j]);
+    }
+}
+
+public:
+    vector<vector<int>> permute(vector<int>& nums) {
+        vector<vector<int>> ans;
+        int index = 0;
+        solve(nums, ans, index);
+        return ans;
+    }
+};
